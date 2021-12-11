69f581685525302813e9dc2712149ab6eef9cddc
Abijith K <79492151+mrastrange@users.noreply.github.com>
2021-12-11 18:51:45 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -281,9 +281,3 @@ if (document.addEventListener) {
     window.event.returnValue = false;
   });
 }
-
-        var cursor = document.querySelector(".cursor");
-        document.addEventListener('mousemove', e=>{
-            cursor.setAttribute("style", "top:" + (e.pageY -10) +"px; left:" + (e.pageX-10)+"px");
-            
-        })  
